2c373b4cf3f467b612941bd5a699bcf4a36505e3
David Fountain <dfountain@sacent.com>
2019-01-22 22:19:27 -0800

    moved admin to views/admin. changed Home controller to pages to be more generic.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get '/admin' => 'home#admin'
+  root 'pages#index'
+  get '/admin' => 'pages#admin'
 end
