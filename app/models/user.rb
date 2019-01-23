bffc4e13569949c70fec85abbfe5b8e9fb2f406d
David Fountain <dfountain@sacent.com>
2019-01-22 22:53:50 -0800

    Implemented User Status to prevent login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
   enum role: [:user, :supervisor, :manager, :admin]
-  
+
   after_initialize :set_default_role, :if => :new_record?
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  def active_for_authentication?
+    super && status?
+  end
+
+  def inactive_message
+    status? ? super : :account_inactive
+  end
 
   def set_default_role
     self.role ||= :user
